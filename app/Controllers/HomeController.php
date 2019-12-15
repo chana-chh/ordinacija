57d3e7cac3cf3034239a730957066768da10a102
chana-chh <chanasrb@gmail.com>
2019-12-15 10:05:24 +0100

    users menu

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,8 +6,9 @@ class HomeController extends Controller
 {
     public function getHome($request, $response)
     {
-        $password="tehnicar";
-        $hash = password_hash($password, PASSWORD_DEFAULT);
+        // $password="vlasnik";
+        // $hash = password_hash($password, PASSWORD_DEFAULT);
+
         // $this->flash->addMessage('success', 'Dobrodošli u Ordinaciju');
         $this->render($response, 'home.twig', compact('password', 'hash'));
     }
